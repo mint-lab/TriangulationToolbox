f62e2e83c8302cc54eb256fc00b02a08978c2f82
Sunglok Choi <sunglok@hanmail.net>
2013-04-05 16:41:17 +0900

    Add MRCLAM dataset

---
+++ run_conv_mrclam.m
@@ -0,0 +1,202 @@
+close all;
+clear all;
+
+disp('== MRCLAM Dataset Conversion for Triangulation Toolbox ==');
+
+% Configure conversion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dataset.robot_n = 5;
+dataset.dir = 'dataset_mrclam/MRCLAM_Dataset1';
+dataset.output = 'dataset_mrclam/mrclam1.mat';
+outlier.distance = 0.5;     % A threshold for outlier distance [m]
+outlier.rotation = 0.5;     % A threshold for outlier bearing [rad]
+threshold.elapse = 1;       % The maximum duration [sec] for being stationary state
+threshold.distance = 0.01;  % The minimum translational change [m] for being stationary state
+threshold.rotation = 0.01;  % The minimum rotational change [rad] for being stationary state
+threshold.landmark = 3;     % The minimum number of landmarks
+histogram.scale = 3;        % The scale of histogram, (width = scale * median)
+histogram.bin = 50;         % The number of bins in drawing histogram
+
+% Convert MRCLAM dataset to landmark-based localization %%%%%%%%%%%%%%%%%%%%%%%
+% Read landmarks and their barcode ID
+landmark = textread([dataset.dir, '/Landmark_Groundtruth.dat'], '', 'delimiter', '\t', 'commentstyle', 'shell');
+landmark = landmark(:,1:3);
+barcode = textread([dataset.dir, '/Barcodes.dat'], '', 'delimiter', '\t', 'commentstyle', 'shell');
+threshold.minId = min(landmark(:,1));
+threshold.maxId = max(landmark(:,1));
+
+% Extract useful measurements for the toolbox
+expNum = 0;
+groundtruth = [];
+measurement = [];
+elapsedTime = [];
+errorDistance = [];
+errorBearing = [];
+for r = 1:dataset.robot_n
+    % 1. Read measurements and their true trajectory
+    prefix = [dataset.dir, '/Robot', num2str(r), '_'];
+    truth = textread([prefix, 'Groundtruth.dat'], '', 'delimiter', '\t', 'commentstyle', 'shell');
+    measure = textread([prefix, 'Measurement.dat'], '', 'delimiter', '\t', 'commentstyle', 'shell');
+    for j = 1:size(barcode,1)
+        measure(measure(:,2) == barcode(j,2),2) = barcode(j,1); % Substitute 'barcode ID' to 'subject ID'
+    end
+    disp([' * Robot #', num2str(r), ': ', num2str(size(measure,1)), ' frames in the measurements.']);
+    disp([' * Robot #', num2str(r), ': ', num2str(size(truth,1)), ' frames in the ground truth.']);
+
+    % 2. Calculate piecewise translation and rotation
+    delta.time = [0; truth(2:end,1) - truth(1:end-1,1)];
+    delta.dist = truth(2:end,2:3) - truth(1:end-1,2:3);
+    delta.dist = [0; sqrt(delta.dist(:,1).^2 + delta.dist(:,2).^2)];
+    delta.rota = [0; truth(2:end,4) - truth(1:end-1,4)];
+    delta.rota(delta.rota >= pi) = delta.rota(delta.rota >= pi) - 2 * pi;
+    delta.rota(delta.rota < -pi) = delta.rota(delta.rota < -pi) + 2 * pi;
+    delta.rota = abs(delta.rota);
+
+    % 3. Find instantaneously stationary sections in the ground truth
+    section.t_index = [];
+    section.t_time = [];
+    start = 1;
+    while start < length(delta.dist)
+        acc.time = 0;
+        acc.dist = 0;
+        acc.rota = 0;
+        for j = (start+1):length(delta.dist)
+            acc.time = acc.time + delta.time(j);
+            acc.dist = acc.dist + delta.dist(j);
+            acc.rota = acc.rota + delta.rota(j);
+            if (acc.time > threshold.elapse) || (acc.dist > threshold.distance) || (acc.rota > threshold.rotation)
+                break;
+            end
+        end
+        if j > (start+1)
+            section.t_index = [section.t_index; start, j-1];
+            section.t_time = [section.t_time; truth(start,1), truth(j-1,1)];
+        end
+        start = start + 1;
+    end
+    disp([' * Robot #', num2str(r), ': ', num2str(size(section.t_index,1)), ' subsequences satisfy time, translation, and orientation conditions.']);
+
+    % 4. Match the stationary sections to the measurements
+    section.m_index = zeros(size(section.t_index));
+    start = 1;
+    for j = 1:size(section.m_index,1)
+        minIndex = inf;
+        maxIndex = 0;
+        for k = start:size(measure,1)
+            if measure(k,1) > section.t_time(j,2)
+                break;
+            elseif measure(k,1) >= section.t_time(j,1)
+                minIndex = min(minIndex, k);
+                maxIndex = max(maxIndex, k);
+            end
+        end
+        section.m_index(j,:) = [minIndex, maxIndex];
+        if ~isinf(minIndex)
+            start = minIndex;
+        end
+    end
+
+    % 5. Select the stationary sections which statisfy the minimum landmark condition
+    selected.truth = [];
+    selected.measure = [];
+    selected.elapse = [];
+    for j = 1:size(section.m_index,1)
+        if ~isinf(section.m_index(j,1)) && (section.m_index(j,2) ~= 0) % If there are landmarks
+            % 5.1. Reject landmarks from other robots
+            sample = measure(section.m_index(j,1):section.m_index(j,2),2:end);
+            sample = sample(sample(:,1) >= threshold.minId,:);
+            sample = sample(sample(:,1) <= threshold.maxId,:);
+
+            % 5.2. Reject outlier data
+            if section.t_index(j,1) == section.t_index(j,2)
+                pose = truth(section.t_index(j,1),2:4);
+            else
+                pose = mean(truth(section.t_index(j,1):section.t_index(j,2),2:4));
+            end
+            map = [];
+            for k = 1:size(sample,1)
+                map = [map; landmark(landmark(:,1) == sample(k,1), 2:end)];
+            end
+            distance = observe_distance([map, zeros(size(map,1),4)], [pose(1:2), 0, 0, 0, pose(3)]);
+            bearing = observe_bearing([map, zeros(size(map,1),4)], [pose(1:2), 0, 0, 0, pose(3)]);
+            diff = sample(:,2:end) - [distance, bearing(:,1)];
+            errorDistance = [errorDistance; diff(:,1)];
+            errorBearing = [errorBearing; diff(:,2)];
+            diff = abs(diff);
+            sample = sample((diff(:,1) < outlier.distance) & (diff(:,2) < outlier.rotation),:);
+
+            % 5.3. Remove duplicated measurements (select the most recent one)
+            table = zeros(threshold.maxId,size(sample,2));
+            table(sample(:,1),:) = sample;
+            mask = zeros(threshold.maxId,1);
+            mask(sample(:,1)) = 1;
+            sample = table(mask == 1,:);
+
+            % 5.4. Accept a section which statisfies the minimum landmark condition
+            if sum(mask) >= threshold.landmark
+                expNum = expNum + 1;
+                selected.truth = [selected.truth; expNum, pose];
+                selected.measure = [selected.measure; expNum * ones(size(sample,1),1), sample];
+                selected.elapse = [selected.elapse; expNum, section.t_time(j,2) - section.t_time(j,1)];
+            end
+        end
+    end
+    disp([' * Robot #', num2str(r), ': ', num2str(size(selected.truth,1)), ' subsequences satisfy the minimal landmark condition.']);
+
+    groundtruth = [groundtruth; selected.truth];
+    measurement = [measurement; selected.measure];
+    elapsedTime = [elapsedTime; selected.elapse];
+end
+disp([' * ', num2str(size(groundtruth,1)), ' subsequences are extracted.']);
+
+% Save landmarks, ground truths, measurements
+landmark(:,1) = landmark(:,1) - threshold.minId + 1;        % Start index from 1
+measurement(:,2) = measurement(:,2) - threshold.minId + 1;  % Start index from 1
+save(dataset.output, 'landmark', 'groundtruth', 'measurement');
+
+% Draw distribution of elapsed time
+maxv = threshold.elapse;
+if isinf(maxv)
+    maxv = histogram.scale * median(elapse_time);
+end
+range = 0:maxv/histogram.bin:maxv;
+count = histc(elapsedTime(:,2), range);
+figure('Color', [1, 1, 1]);
+hold on;
+    set(gca, 'FontSize', 12);
+    set(gca, 'YTick', []);
+    box on;
+    grid on;
+    bar(range, count, 'style', 'histc');
+    xlabel('Elapsed Time', 'FontSize', 12);
+    ylabel('Frequency', 'FontSize', 12);
+hold off;
+
+% Draw distribution of distance error
+medv = median(abs(errorDistance));
+range = -histogram.scale*medv:2*histogram.scale*medv/histogram.bin:histogram.scale*medv;
+count = hist(errorDistance, range);
+figure('Color', [1, 1, 1]);
+hold on;
+    set(gca, 'FontSize', 12);
+    set(gca, 'YTick', []);
+    box on;
+    grid on;
+    bar(range, count, 'style', 'hist');
+    xlabel('Distance Error [m]', 'FontSize', 12);
+    ylabel('Frequency', 'FontSize', 12);
+hold off;
+
+% Draw distribution bearing error
+medv = median(abs(errorBearing));
+range = -histogram.scale*medv:2*histogram.scale*medv/histogram.bin:histogram.scale*medv;
+count = hist(errorBearing, range);
+figure('Color', [1, 1, 1]);
+hold on;
+    set(gca, 'FontSize', 12);
+    set(gca, 'YTick', []);
+    box on;
+    grid on;
+    bar(tran_rad2deg(range), count, 'style', 'hist');
+    xlabel('Bearing Error [deg]', 'FontSize', 12);
+    ylabel('Frequency', 'FontSize', 12);
+hold off;
---
+++ run_eval_mrclam.m
@@ -0,0 +1,93 @@
+close all;
+clear all;
+
+disp('== Localization Evaluatioin (with MRCLAM Dataset) for Triangulation Toolbox ==');
+
+% Configure experiments %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+config.dataFile = ... % A list of data files
+{                                  ...
+    'dataset_mrclam/mrclam1.mat'; ...
+    'dataset_mrclam/mrclam2.mat'; ...
+    'dataset_mrclam/mrclam3.mat'; ...
+    'dataset_mrclam/mrclam4.mat'; ...
+    'dataset_mrclam/mrclam5.mat'; ...
+    'dataset_mrclam/mrclam6.mat'; ...
+    'dataset_mrclam/mrclam7.mat'; ...
+    'dataset_mrclam/mrclam8.mat'; ...
+    'dataset_mrclam/mrclam9.mat'; ...
+};
+config.pose = [];
+config.algorithm = ... % Description of localization algorithms
+{                                                                                                       ...
+  % #, Dim, Name,         Local. Function,      Observation Function,     Min. N, Valid,    Line Sytle; ...
+    1,  2,  'Sayed05-2D', @localize2d_sayed05,  @observe_distance,             3, [1 1 0 0 0 0], 'kx-'; ...
+    2,  2,  'Betke97',    @localize2d_betke97,  @observe_bearing,              3, [1 1 0 0 0 1], 'gd-'; ...
+    3,  2,  'Shim02-Alg', @localize2d_shimshoni02_algebraic, @observe_bearing, 3, [1 1 0 0 0 1], 'b--'; ...
+    4,  2,  'Shim02-Imp', @localize2d_shimshoni02_improved,  @observe_bearing, 3, [1 1 0 0 0 1], 'b+-'; ...
+    5,  2,  'Se05',       @localize2d_se05,     @observe_displacement,         2, [1 1 0 0 0 1], 'rs-'; ...
+};
+config.matFile = 'run_eval_mrclam.mat';
+
+criteria.name = {'Position Error [m]', 'Orientation Error [deg]', ...
+                 'Computing Time [msec]', 'Number of Failures'}; % Name of evaluation criteria
+criteria.repr = {@median, @median, @median, @sum}; % Functions for calculating representive values
+                                                   %  (e.g. mean, median, std, and sum)
+criteria.format = {'%.6f', '%.3f', '%.6f', '%d'};  % Format for printing text
+
+% Perform experiments %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+config.algoDims = 2;
+config.algoName = 3;
+config.algoEstm = 4;
+config.algoObsv = 5;
+config.algoMinN = 6;
+config.algoVald = 7;
+config.algoLine = 8;
+config.algoSelM = 1:size(config.algorithm,1);
+
+for m = 1:size(config.algorithm,1)
+    obsFuncName{m} = func2str(config.algorithm{m,config.algoObsv});
+end
+record.perf{1,1} = [];
+record.pose{1,1} = [];
+for d = 1:size(config.dataFile,1)
+    % 1. Read each data file which contains landmarks, ground truths, and measurements
+    data = load(config.dataFile{d});
+    for t = 1:size(data.groundtruth,1)
+        truth = [data.groundtruth(t,2:3), zeros(1,3), data.groundtruth(t,4)];
+        measure = [data.measurement(data.measurement(:,1) == t,2:end)];
+        map = [data.landmark(measure(:,1),2:end), zeros(size(measure,1),4)];
+        set.perf = zeros(1,4,size(config.algorithm,1));
+        set.pose = zeros(1,6,size(config.algorithm,1));
+        for m = config.algoSelM
+            % 2. Rearrange measurements for each algorithm
+            if isequal(obsFuncName{m}, 'observe_distance')
+                obsData = measure(:,2);
+            elseif isequal(obsFuncName{m}, 'observe_bearing')
+                obsData = [measure(:,3), zeros(size(measure,1),1)];
+            elseif isequal(obsFuncName{m}, 'observe_displacement')
+                obsData = repmat(measure(:,2),1,3) .* ...
+                    [cos(measure(:,3)), sin(measure(:,3)), zeros(size(measure,1),1)];
+            else
+                error(['This dataset cannot be applied to the algorithm #', num2str(m), '!']);
+            end
+
+            % 3. Perform each algorithm
+            tic;
+            [pose, valid] = feval(config.algorithm{m,config.algoEstm}, obsData, map);
+            elapse = toc * 1000; % [sec] to [msec]
+            set.perf(1,1,m) = error_position(truth(1:3), pose(1:3));
+            set.perf(1,2,m) = tran_rad2deg(error_orientation(truth(4:6), pose(4:6))); % [rad] to [deg]
+            set.perf(1,3,m) = elapse;
+            set.perf(1,4,m) = ~isequal(valid, config.algorithm{m,config.algoVald});
+            set.pose(1,:,m) = pose;
+        end
+        record.perf{1,1} = [record.perf{1,1}; set.perf];
+        record.pose{1,1} = [record.pose{1,1}; set.pose];
+    end
+    disp([' * Experiment on dataset #', num2str(d), ' is complete.']);
+end
+
+% 4. Save experimental results
+save(config.matFile, 'config', 'criteria', 'record');
+
+% To visualize the result, please use the script, 'run_draw_distribution', with 'target.ex = 1' and 'target.v = 1'.
